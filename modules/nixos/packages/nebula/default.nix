09c9a5c35b9db5a6a4369772eb5b8ab724fabcb8
SirBerg <benno@boerg.co>
2024-10-19 00:02:48 +0200

    Added mini pc config

---
+++ modules/nixos/packages/nebula/default.nix
@@ -0,0 +1,19 @@
+{ options, config, lib, pkgs, ...}:
+
+with lib;
+with lib.types;
+let
+	cfg = config.boerg.packages.nebula;
+in
+{
+	options.boerg.packages.nebula.enable = mkOption {
+		type = bool;
+		default = false;
+	};
+	config = mkIf cfg.enable {
+
+		environment.systemPackages = with pkgs;
+		[
+		];
+	};
+}
