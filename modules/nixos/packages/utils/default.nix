f524de28a003c126ab92fc982c2e0eff9d312218
SirBerg <benno@boerg.co>
2024-11-18 21:31:13 +0100

    I do not know anymore

--- modules/nixos/packages/utils/default.nix
+++ modules/nixos/packages/utils/default.nix
@@ -45,7 +45,7 @@ in
             mplayer
             #gitbutler
             audacity
-            multiviewer-for-f1
+            #multiviewer-for-f1
             attic-client
 		];
 		programs.coolercontrol.nvidiaSupport = true;
