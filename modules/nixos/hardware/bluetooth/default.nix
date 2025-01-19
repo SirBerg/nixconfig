744324e89a8114dd6cdd3146176caed810a49d8e
SirBerg <benno@boerg.co>
2025-01-19 19:15:43 +0100

    Cleaned up some options

---
+++ modules/nixos/hardware/bluetooth/default.nix
@@ -0,0 +1,28 @@
+#Common Nix Packages
+{ options, config, lib, pkgs, ...}:
+
+with lib;
+with lib.types;
+let
+	cfg = config.boerg.hardware.bluetooth;
+in
+{
+	options.boerg.hardware.bluetooth.enable = mkOption {
+		type = bool;
+		default = false;
+	};
+	config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs;
+      [
+        bluetuith
+      ];
+    hardware.bluetooth = {
+      enable = true;
+      settings = {
+        General = {
+          Experimental = true;
+        };
+      };
+    };
+	};
+}
---
+++ modules/nixos/virtualisation/docker/default.nix
@@ -0,0 +1,28 @@
+#Common Nix Packages
+{ options, config, lib, pkgs, ...}:
+
+with lib;
+with lib.types;
+let
+	cfg = config.boerg.virt.docker;
+in
+{
+	options.boerg.virt.docker.enable = mkOption {
+		type = bool;
+		default = false;
+	};
+	config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs;[
+      docker-compose
+    ];
+    virtualisation.docker = {
+      enable = true;
+      daemon.settings = {
+        "log-driver" = "json-file";
+        "log-opts" = {
+          "tag" = "{{.Name}}";
+        };
+      };
+    };
+	};
+}
