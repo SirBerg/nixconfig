056a96fe5ac56519d496d3c2ba4940b6ba492928
SirBerg <benno@boerg.co>
2025-04-26 17:30:15 +0200

    Add new k3s node

--- systems/x86_64-linux/warmind-targe/default.nix
+++ systems/x86_64-linux/warmind-targe/default.nix
@@ -35,7 +35,7 @@
     config.standard.enable = true;
     kubernetes = {
         enable = true;
-        role = "server";
+        role = "agent";
         address = "10.124.0.3";
         init = false;
     };
