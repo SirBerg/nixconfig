04cf265e1225ffccd814124dacf2e4ae16b3cbe8
SirBerg <benno@boerg.co>
2025-05-07 16:45:09 +0200

    Add packages to satou

--- systems/aarch64-darwin/satou/default.nix
+++ systems/aarch64-darwin/satou/default.nix
@@ -7,7 +7,19 @@
       environment.systemPackages =
         [ pkgs.vim
         ];
-
+      boerg = {
+            packages = {
+              common.enable = true;
+              fonts.enable = true;
+              steam.enable = true;
+              development.enable = true;
+              utils.extended.enable = true;
+              utils.gui.enable = true;
+            };
+            docker = {
+              enable = true;
+            };
+      };
       # Auto upgrade nix package
       # nix.package = pkgs.nix;
 
