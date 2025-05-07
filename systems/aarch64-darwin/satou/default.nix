6ce9da3367df3fc1b62abd436fda03a62c07bfcd
SirBerg <benno@boerg.co>
2025-05-07 16:24:39 +0200

    Added darwin system

--- systems/aarch64-darwin/satou/default.nix
+++ systems/aarch64-darwin/satou/default.nix
@@ -3,22 +3,31 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, self, ... }:
+let
+  configuration = { pkgs, ... }: {
+    programs.zsh.enable = true;
 
+    # Set your time zone.
+    time.timeZone = "Europe/Berlin";
+    # Install firefox.
+    programs.firefox.enable = true;
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      wget
+    ];
+    system.stateVersion = "24.05"; # Did you read the comment?
+  };
+in
 {
-  programs.zsh.enable = true;
+    darwinConfigurations."satou" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
 
-  # Set your time zone.
-  time.timeZone = "Europe/Berlin";
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    wget
-  ];
-  system.stateVersion = "24.05"; # Did you read the comment?
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."satou".pkgs;
 }
