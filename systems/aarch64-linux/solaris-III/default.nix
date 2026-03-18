92f73d5c8b426b923729cf956f117fbce00f9074
SirBerg <benno@boerg.co>
2026-03-18 12:16:48 +0100

    update solaris

--- systems/aarch64-linux/solaris-III/default.nix
+++ systems/aarch64-linux/solaris-III/default.nix
@@ -56,7 +56,7 @@
 	    DB_FILE="/var/lib/geoip/dbip-country.csv"
 	    YEAR=$(date +%Y)
 		MONTH=$(date +%m)
-		DB_URL="https://download.db-ip.com/free/dbip-country-lite-${YEAR}-${MONTH}.csv.gz"
+		DB_URL="https://download.db-ip.com/free/dbip-country-lite-''${YEAR}-''${MONTH}.csv.gz"
 	    COUNTRIES=("DE" "AT" "CH")
 
 	    mkdir -p /var/lib/geoip
