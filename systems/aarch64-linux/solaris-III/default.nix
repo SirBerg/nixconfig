b5228c3cc921c6d46ffc0fff482d9266b45c5e03
SirBerg <benno@boerg.co>
2026-03-18 12:10:54 +0100

    update solaris

--- systems/aarch64-linux/solaris-III/default.nix
+++ systems/aarch64-linux/solaris-III/default.nix
@@ -53,8 +53,9 @@
 	    #!/usr/bin/env bash
 	    set -euo pipefail
 
-	    DB_URL="https://download.db-ip.com/free/dbip-country-lite-$(date +%Y-%m).csv.gz"
-	    DB_FILE="/var/lib/geoip/dbip-country.csv"
+	    DB_FILE=YEAR=$(date +%Y)
+		MONTH=$(date +%m)
+		DB_URL="https://download.db-ip.com/free/dbip-country-lite-${YEAR}-${MONTH}.csv.gz""/var/lib/geoip/dbip-country.csv"
 	    COUNTRIES=("DE" "AT" "CH")
 
 	    mkdir -p /var/lib/geoip
