4b12fae5a1b4679f95dd89d8895237eafd79f241
SirBerg <benno@boerg.co>
2026-03-18 11:34:11 +0100

    update solaris

--- systems/aarch64-linux/solaris-III/default.nix
+++ systems/aarch64-linux/solaris-III/default.nix
@@ -163,7 +163,7 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
   networking.firewall.extraInputRules = ''
-	INPUT -s 46.128.11.17 -j DROP
+	INPUT 46.128.11.17 DROP
   '';
   # Install firefox.
   programs.firefox.enable = true;
