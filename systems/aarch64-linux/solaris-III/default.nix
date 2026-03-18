0633cac071179bb314b02eb5e09f278cebbce521
SirBerg <benno@boerg.co>
2026-03-18 10:38:50 +0100

    update solaris

--- systems/aarch64-linux/solaris-III/default.nix
+++ systems/aarch64-linux/solaris-III/default.nix
@@ -171,7 +171,8 @@
   gzip
   nftables
   ];
-boot.kernelParams = [ "console=tty0" "console=ttyS0,9600n8" ];
+boot.kernelParams = [ "console=tty1" ];
+systemd.services."getty@tty1".enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
